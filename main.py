3c56766712f3716dd960247ce5b22035f20c36af
edilson <eddylson.londonio@gmail.com>
2020-11-29 11:07:43 -0500

    print removed

--- main.py
+++ main.py
@@ -5,46 +5,43 @@ import sys
 from project.dualsimplex import dual_simplex
 
 
-
-
-
-objetive_function = sys.argv[1]
+objective_function = sys.argv[1]
 constraints = sys.argv[2:]
-print(objetive_function)
-print(constraints)
+
+
 def get_variables_and_coefficients(eq):
 
-    variables = re.findall(r'[a-z]+',eq)
-    coefficients = re.split(r'[a-z]+',eq)
-    integer_coefficients = list(map(lambda v : int(v), coefficients[0:-1]))
-    equal = re.split(r'=|<|>|(<&=)|(>&=)'  ,eq)
+    variables = re.findall(r"[a-z]+", eq)
+    coefficients = re.split(r"[a-z]+", eq)
+    integer_coefficients = list(map(lambda v: int(v), coefficients[0:-1]))
+    equal = re.split(r"=|<|>|(<&=)|(>&=)", eq)
     if len(equal) > 1:
         return variables, integer_coefficients, float(equal[-1])
-    return variables , coefficients[0:-1]
+    return variables, coefficients[0:-1]
 
 
 def main():
 
     try:
-        values_objetive_function = get_variables_and_coefficients(
-                objetive_function
-                )
-        print(values_objetive_function)
-        coefficients_objetive_function = values_objetive_function[1]
+        values_objective_function = get_variables_and_coefficients(objective_function)
+        coefficients_objective_function = values_objective_function[1]
         constraints_coefficients = []
         constraints_values = []
         for c in constraints:
-            _,coefficients , result = get_variables_and_coefficients(c)
-            print(coefficients)
+            _, coefficients, result = get_variables_and_coefficients(c)
             constraints_coefficients.append(coefficients)
             constraints_values.append(result)
-        print(constraints_coefficients, constraints_values)
     except Exception as e:
-        print(e)
-        print('Revisar las funciones de entrada')
+        print("Revisar las funciones de entrada")
+        return
     finally:
-        result = dual_simplex(coefficients_objetive_function,constraints_coefficients,constraints_values)
+        result = dual_simplex(
+            coefficients_objective_function,
+            constraints_coefficients,
+            constraints_values,
+        )
         print(result)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- project/dualsimplex.py
+++ project/dualsimplex.py
@@ -1,22 +1,22 @@
 from scipy.optimize import linprog
-import numpy as np 
+import numpy as np
 
-def dual_simplex(objetive,constraints,values):
+
+def dual_simplex(objective, constraints, values):
     A = np.array(constraints)
     b = np.array(values)
-    c = np.array(objetive)
-    result = linprog(c,A_ub=A,b_ub=b,bounds=(0,None),method="revised simplex")
+    c = np.array(objective)
+    result = linprog(c, A_ub=A, b_ub=b, bounds=(0, None), method="revised simplex")
     return result
 
 
+# result = dual_simplex([3,2,1],[[-3,-1,-1],[3,-3,-1],[1,1,1],[-1,0,0],[0,-1,0],[0,0,-1]],[-3,-6,3,0,0,0])
+# print(result)
 
-#result = dual_simplex([3,2,1],[[-3,-1,-1],[3,-3,-1],[1,1,1],[-1,0,0],[0,-1,0],[0,0,-1]],[-3,-6,3,0,0,0])
-#print(result)
-
-#A  = np.array([[-3,-1,-1],[3,-3,-1],[1,1,1],[-1,0,0],[0,-1,0],[0,0,-1]])
-#b = np.array([-3,-6,3,0,0,0])
-#c = np.array([3,2,1])
+# A  = np.array([[-3,-1,-1],[3,-3,-1],[1,1,1],[-1,0,0],[0,-1,0],[0,0,-1]])
+# b = np.array([-3,-6,3,0,0,0])
+# c = np.array([3,2,1])
 #
 #
-#result = linprog(c,A_ub=A,b_ub=b,bounds=(0,None),method="revised simplex")
-#print(result)
+# result = linprog(c,A_ub=A,b_ub=b,bounds=(0,None),method="revised simplex")
+# print(result)
